f27c6ccb5c2302208e957e2327b108e86f985f71
Ione Souza Junior <junior@ionixjunior.com.br>
2016-05-19 19:40:52 -0300

    Adicionado serviço estático para listar contatos

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,7 @@
 angular.module('starter.controllers', [])
 
-.controller('ContactsCtrl', function($scope) {
-  
+.controller('ContactsCtrl', function($scope, ContactService) {
+    $scope.contacts = ContactService.all();
 })
 
 .controller('ContactDetailCtrl', function($scope) {
--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,21 @@
 angular.module('starter.services', [])
 
+.factory('ContactService', function () {
+    var contacts = [{
+        first_name: 'Ione',
+        last_name: 'Souza Junior',
+        email: 'junior@ionixjunior.com.br'
+    }, {
+        first_name: 'João',
+        last_name: 'Silva',
+        email: 'joaosilva@gmail.com'
+    }];
 
+    return {
+        all: function () {
+            return contacts;
+        }
+    };
+})
 
 ;
